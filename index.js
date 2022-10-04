90a7d43c1f22c2296329880c7868db2b56d72def
mawa <mawahama05@gmail.com>
2022-10-03 17:47:41 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution in this file!
+// var customerName = 'bob';
+// function upperCaseCustomerName(){
+//   customerName = customerName.toUpperCase()
+// return customerName
+// }
+// console.log(upperCaseCustomerName());
+// // console.log(customerName);
+// // console.log(upperCaseCustomerName());
+// function setBestCustomer(){
+//     var bestCustomer = "not bob"
+//     return bestCustomer
+// }
+// console.log(setBestCustomer());
+// function overwriteBestCustomer(){
+//     var bestCustomer = "maybe bob"
+//     return bestCustomer
+// }
+// const changeLeastFavoriteCustomer = "poor someone"
+// function changeLeastFavorieCustomer(){
+//     const leastFavoriteCustomer = "ik i can't change"
+// }
+var customerName = 'bob'
+const leastFavoriteCustomer = 'wtvr'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'billy'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
